2ee06c84ea1efc1e6de44152a8d26a80c345c3a8
ashwani2529 <ashwanix2749@gmail.com>
2025-02-23 17:36:17 +0530

    added ml

--- frontend/src/components/Join/Join.js
+++ frontend/src/components/Join/Join.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Join.css";
 import logo from "../../images/logo.png";
 import { Link } from "react-router-dom";
